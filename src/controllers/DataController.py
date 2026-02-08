4189ff4835821e01c5871fc255a09cf907c178d6
Brahim Ouhammou <bouhammo@c1r2p10.1337.ma>
2026-02-08 21:47:53 +0100

    update

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,23 +1,26 @@
 from .BaseController import BaseController
 from fastapi import UploadFile
-
+from models  import ResponseSignal
 
 class DataController(BaseController):
     def __init__(self):
         super().__init__()
         self.size_scale = 1048576
+        
 
     def validate_upload_file(self, file: UploadFile):
         allowed_types = self.app_setting.FILE_ALLOWED_TYPE.split(",")
+        # print("ALLOWED TYPES:", allowed_types)
+        print("CONTENT TYPE:", file.content_type)
 
         if file.content_type not in allowed_types:
-            return False
+            return False , ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
 
         contents = file.file.read()
         size = len(contents)
         file.file.seek(0)
 
         if size > self.app_setting.FILE_MAX_SIZE * self.size_scale:
-            return False
+            return False , ResponseSignal.FILE_SIZE_EXCEEDED.value
 
-        return True
+        return True  , ResponseSignal.FILE_VALIDATED_SUCCESS.value
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, APIRouter , Depends , UploadFile , File
+from fastapi import FastAPI, APIRouter , Depends , UploadFile , File , status
+from  fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings , Settings
-from controllers import DataController
+from controllers import DataController , ProjectController
+import aiofiles
+from models import ResponseSignal
 
 
 data_router = APIRouter(
@@ -9,16 +12,37 @@ data_router = APIRouter(
     tags=["api_v1" , "data"]
 )
 
-from fastapi import UploadFile, File
 
 @data_router.post("/upload/{project_id}")
-def upload_data(
+async def upload_data(
     project_id: str,
     file: UploadFile = File(...),
 ):
-    is_valid = DataController().validate_upload_file(file)
-    return {"valid": is_valid}
+    is_valid  , result_signal = DataController().validate_upload_file(file)
+    
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=
+                {
+                    "result signal": result_signal,
+                    "valid": is_valid
+                })
 
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    
+    file_path = os.path.join(project_dir_path,file.filename)
+    
+    async with aiofiles.open(file_path ,"wb") as f:
+        while chunk := await file.read(app_setting.FILE_DEFAULT_CHUNK_SIZE):
+            await f.write(chunk)
+
+    # print(f"file_path = {file_path}\n\n")
+    return JSONResponse(
+        content=
+            {
+                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            })
 
 
 
